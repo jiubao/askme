9bbf3fa882fb8a2d8b26d76d65b543d18318e98d
jiubao <xia.tlbb@gmail.com>
2015-12-25 15:15:30 +0800

    askme data source

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # askme
 frontend question? try ask me.
+
+a search page, for front end issues
+will coming
---
+++ data.js
@@ -0,0 +1,43 @@
+var source = [{
+  name: "fontforge",
+  keys: "icon font, font, icon, ligature",
+  desc: "ligature",
+  urls: [urls.ligature, urls.googlematerialicons, urls.fontclock, urls.fontforge, urls.materializecss]
+}, {
+  name: "highlight",
+  keys: "highlight, syntax, code, pre",
+  desc: "hightlight code",
+  urls: [urls.prismjs, urls.highlightjs]
+}, {
+  name: "jstemplate",
+  keys: "template",
+  desc: "js template",
+  urls: [urls.ejs]
+}, {
+  name: "aframe",
+  keys: "vr, 3d",
+  desc: "mozilla open source vr framework for web dev",
+  urls: [urls.aframe]
+}, {
+  name: "jsquiz",
+  keys: "quiz, js",
+  desc: "",
+  urls: [urls["kangax.jsquiz"], urls["kangax.es6quiz"], urls["leftstick.jsquiz"]]
+}];
+
+var urls = {
+  "googlematerialicons": "//google.github.io/material-design-icons/",
+  "ligature": "//alistapart.com/article/the-era-of-symbol-fonts",
+  "fontclock": "//timepiece.inostudio.de/",
+  "fontforge": "//fontforge.github.io/",
+  "designwithfontforge": "//designwithfontforge.com/en-US/index.html",
+  "prismjs": "//prismjs.com/",
+  "highlightjs": "//highlightjs.org/",
+  "ejs": "//github.com/tj/ejs",
+  "materializecss": "//materializecss.com/",
+  "aframe": "//aframe.io/",
+  "leftstick.jsquiz": "http://leftstick.github.io/quiz-legend/",
+  "kangax.jsquiz": "http://perfectionkills.com/javascript-quiz/",
+  "kangax.es6quiz": "http://perfectionkills.com/javascript-quiz-es6/"
+
+}
