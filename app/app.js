c319c8a4f345d2c2fe4a589d3d5589a2afdac900
jiubao <xia.tlbb@gmail.com>
2015-12-29 15:55:49 +0800

    add injector

--- app/app.js
+++ app/app.js
@@ -1,14 +1,45 @@
-$(document).ready(function() {
+var $injector = window.$j = window.$injector = createInjector();
+var $define = $injector.define;
+
+$define('enter', ['simple', function() {
+  console.log("app running");
+}]);
+
+$define('simple', ['detail', function(detail) {
   var $sinput = $('#sinput');
   var $sinputContainer = $('.sinput');
   var $sbox = $('#sbox');
-  var $dbox = $('#dbox');
-  $sinput.on('focus', function() {
-    $sinputContainer.addClass('inputFocus');
-  }).on('blur', function() {
-    $sinputContainer.removeClass('inputFocus');
-  }).on('keydown', function() {
+
+  init();
+  return {};
+
+  function init() {
+    $sinput.on('focus', function() {
+      $sinputContainer.addClass('inputFocus');
+    }).on('blur', function() {
+      $sinputContainer.removeClass('inputFocus');
+    }).on('keydown', function() {
+      hide();
+      detail.show();
+    });
+  }
+
+  function hide() {
     $sbox.css('display', 'none');
-    $dbox.css('display', 'block');
-  });
+  }
+}]);
+
+$define('detail', function() {
+  var $dbox = $('#dbox');
+  return {
+    show: function() {
+      $dbox.css('display', 'block');
+    }
+  };
+});
+
+
+
+$(document).ready(function() {
+  $injector.get('enter');
 });
